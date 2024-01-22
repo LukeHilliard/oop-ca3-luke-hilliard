7e73f58bab87a01c791b663ef1d751be730b00f4
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-01-22 19:42:39 +0000

    Main logic for finding identifiers and storing them in the map.
    I am using a List of Integers to store the line numbers. The List will maintain the order values were added and can dynamically change.
    
    TODO
    Correctly track the line number. Currently, the line counter increase after each iteration of the while-loop.

--- CA3_Question3.java
+++ CA3_Question3.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *  Name: Luke Hilliard
@@ -14,14 +14,45 @@ public class CA3_Question3
        File file = new File(fileName);
        Scanner sc = new Scanner(file);
 
-       while(sc.hasNext()) {
+       // tokenize the scanner
+       sc.useDelimiter("[^A-Za-z0-9_]+");
 
+
+       int lineNumber = 0;
+
+       // A map with the identifier as the key and a List of integers to keep track of the multiple line numbers an identifier may occur on
+       Map<String, List<Integer>> identifierMap = new HashMap<>();
+
+       while(sc.hasNext()) {
+            String identifier = sc.next();
+
+            //TODO correctly track the line number
+            lineNumber++;
+
+            // if the found identifier is not already in the map, add it
+            if(!identifierMap.containsKey(identifier)) {
+                identifierMap.put(identifier, new ArrayList<>());
+            }
+
+            // get the corresponding entry from the map using the key and add the line number to the List
+            identifierMap.get(identifier).add(lineNumber);
        }
 
+        System.out.println("----- Java Identifier Count -----");
+        System.out.println("From -> " + fileName + "\n");
+
+        // Display Identifiers and their line numbers
+        for(Map.Entry<String, List<Integer>> entry : identifierMap.entrySet()) {
+            String key = entry.getKey();
+            List<Integer> lineNumbers = entry.getValue();
+
+            System.out.println(key + " -> " + lineNumbers.toString());
+        }
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        readFile("Question2.java");
+    public static void main(String[] args)throws IOException {
+        readFile("CA3_Question1.java");
+
     }
 }
