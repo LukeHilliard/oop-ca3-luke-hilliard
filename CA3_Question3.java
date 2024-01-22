01a93637fcd2e1eb6c12027f62cda17437e27c82
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-01-22 19:05:27 +0000

    Linked a Scanner to the new File class created from the inputted file name.
    Body of while loop added
    condition: while the Scanner is still scanning the file

--- CA3_Question3.java
+++ CA3_Question3.java
@@ -1,17 +1,27 @@
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Scanner;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Luke Hilliard
+ *  Class Group: SD2b
  */
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
+
+    public static void readFile(String fileName) throws FileNotFoundException
     {
+       File file = new File(fileName);
+       Scanner sc = new Scanner(file);
+
+       while(sc.hasNext()) {
+
+       }
+
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile("src/CA3_Question1.java");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        readFile("Question2.java");
     }
 }
