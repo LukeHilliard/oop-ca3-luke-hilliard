9d660b06d2dd7193f55b075f511f62aa2ee2bef5
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-01-23 19:00:44 +0000

    Initial start of the question.
    Program now takes validated command inputs.

--- CA3_Question5.java
+++ CA3_Question5.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /**
  *  Name:
  *  Class Group:
@@ -9,5 +13,37 @@ public class CA3_Question5
     public static void main(String[] args)
     {
 
+        Queue<String> takeoffQueue = new LinkedList<String>();
+        Queue<String> landingQueue = new LinkedList<String>();
+
+        boolean exit = false;
+        Scanner kb = new Scanner(System.in);
+        String command = "";
+
+        System.out.println("----- Flight Control Tower -----\n");
+
+        do {
+            System.out.print("Command: ");
+            command = handleCommandInput(kb.nextLine());
+
+            System.out.println("Entered Command -> " + command);
+
+        }while(!exit);
     }
+
+    public static String handleCommandInput(String input) {
+        Scanner kb = new Scanner(System.in);
+
+       while(!checkCommandInput(input)) {
+           System.out.println("*** Invalid command --> " + input + " ***\n");
+           System.out.print("Command: ");
+           input = kb.nextLine();
+       }
+        return input;
+    }
+    public static boolean checkCommandInput(String input) {
+        return input.equalsIgnoreCase("takeoff") || input.equalsIgnoreCase("land") ||
+                input.equalsIgnoreCase("next") || input.equalsIgnoreCase("quit");
+    }
+
 }
