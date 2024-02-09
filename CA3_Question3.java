a14c7860eea5be1326d1e23ee03dd8cfada69333
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-02-09 20:49:33 +0000

    Final fix-up
    Correctly tracks the line number

--- CA3_Question3.java
+++ CA3_Question3.java
@@ -14,29 +14,28 @@ public class CA3_Question3
        File file = new File(fileName);
        Scanner sc = new Scanner(file);
 
-       // tokenize the scanner
-       sc.useDelimiter("[^A-Za-z0-9_]+");
+        // A map with the identifier as the key and a List of integers to keep track of the multiple line numbers an identifier may occur on
+        Map<String, List<Integer>> identifierMap = new HashMap<>();
+        int lineNumber = 0;
 
-
-       int lineNumber = 0;
-
-       // A map with the identifier as the key and a List of integers to keep track of the multiple line numbers an identifier may occur on
-       Map<String, List<Integer>> identifierMap = new HashMap<>();
-
-       while(sc.hasNext()) {
-            String identifier = sc.next();
-
-            //TODO correctly track the line number
+        while(sc.hasNext()) { // while the LINE has next
             lineNumber++;
+            String line = sc.nextLine();
+            Scanner lineScnr = new Scanner(line);
+            lineScnr.useDelimiter("[^A-Za-z0-9_]+");   // tokenize the scanner
 
-            // if the found identifier is not already in the map, add it
-            if(!identifierMap.containsKey(identifier)) {
-                identifierMap.put(identifier, new ArrayList<>());
+            while(lineScnr.hasNext()) // while the WORD has next
+            {
+               String identifier = lineScnr.next();
+               // if the found identifier is not already in the map, add it
+               if(!identifierMap.containsKey(identifier)) {
+                   identifierMap.put(identifier, new ArrayList<>());
+               }
+               // get the corresponding entry from the map using the key and add the line number to the List
+               identifierMap.get(identifier).add(lineNumber);
             }
-
-            // get the corresponding entry from the map using the key and add the line number to the List
-            identifierMap.get(identifier).add(lineNumber);
        }
+
 
         System.out.println("----- Java Identifier Count -----");
         System.out.println("From -> " + fileName + "\n");
