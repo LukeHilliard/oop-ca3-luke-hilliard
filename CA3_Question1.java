94ad22b9b97fd88b62814689b9a1ddab7f7b435a
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-02-11 17:09:46 +0000

    stashed changes

--- CA3_Question1.java
+++ CA3_Question1.java
@@ -3,22 +3,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class CA3_Question1 {
-
-    /*
-     * A homeowner rents out parking spaces in a driveway during special events.
-     * The driveway isa “last-in, first-out” LIFO stack. Of course when a car owner retrieves a vehicle that was not the last one in, the cars blocking it must temporarily move to the street so that the
-     * requested vehicle can leave.
-     *
-     * Write a program that models this behaviour, using one stack for
-     * the driveway and one stack for the street.
-     * Use integer values as license plate numbers (e.g. 1,2,3,4…).
-     * Positive numbers add a car (1,2,3…),
-     * negative numbers remove a car(-2,-1,…),
-     * zero stops the simulation.
-     *
-     * Print out the stack after each operation is complete. So, entering “1” means – add car number 1 to the driveway, entering “-2” means - retrieve car number 2 from the driveway.
-     */
-
     public static void main(String[] args) {
         Stack<Integer> driveway = new Stack<>();
         Stack<Integer> street = new Stack<>();
